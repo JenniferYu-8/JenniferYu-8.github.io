76339f283b19963fd1cb20cb411dd5924b33c4a3
JenniferYu8 <87811606+JenniferYu8@users.noreply.github.com>
2023-05-16 12:34:36 -0400

    faq page layout

--- faq/script.js
+++ faq/script.js
@@ -14,8 +14,8 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
-    // this.classList.toggle("active");
-    // acc[i].firstElementChild.classList.toggle("active"); // doesn't work
+    this.classList.toggle("clear");
+    this.querySelector(".fa-angle-down").classList.toggle("active"); // doesn't workx
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
