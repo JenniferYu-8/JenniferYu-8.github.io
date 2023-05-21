7066116d1667c7c1fee6356a50e031078f2a16b0
JenniferYu-8 <98477492+JenniferYu-8@users.noreply.github.com>
2023-05-21 16:11:04 -0400

    Revert "About section"

--- secretariat/script.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = () => {
-    const nav = document.querySelector('#fixed');
-    if (this.scrollY <= 100) {
-      nav.className = '';
-    } else {
-      nav.className = 'scroll';
-    }
-  };
