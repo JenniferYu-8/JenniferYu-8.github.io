883771f5c35387c1c801f7cfb53beb6fde8005cd
JenniferYu8 <87811606+JenniferYu8@users.noreply.github.com>
2023-10-14 19:36:53 -0400

    add hotel page (non-responsive)

---
+++ hotel/script.js
@@ -0,0 +1,34 @@
+window.onscroll = () => {
+  const nav = document.querySelector('#fixed');
+  if (this.scrollY <= 100) {
+    nav.className = '';
+  } else {
+    nav.className = 'scroll';
+  }
+};
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+acc[i].addEventListener("click", function() {
+  /* Toggle between adding and removing the "active" class,
+  to highlight the button that controls the panel */
+  this.classList.toggle("clear");
+  this.querySelector(".fa-angle-down").classList.toggle("active"); // doesn't workx
+
+  /* Toggle between hiding and showing the active panel */
+  var panel = this.nextElementSibling;
+  if (panel.style.display === "block") {
+    panel.style.display = "none";
+  } else {
+    panel.style.display = "block";
+  }
+
+  if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+});
+}
